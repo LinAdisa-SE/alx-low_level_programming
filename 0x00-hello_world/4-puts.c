65b80242334f7e3dfb93c799d3b1a9f7d214e4cd
Linda Adisa Masitsa <lindaadisa022@gmail.com>
2022-06-02 13:45:45 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+nclude <stdio.h>
 
 
 
@@ -13,10 +13,9 @@
  *     */
 
 int main(void)
-
 {
-	puts("\"Programming is like building a multilingual puzzle");
+		puts("\"Programming is like building a multilingual puzzle");
 
-	    return (0);
+	    		return (0);
 
 }
