787d77c4c8f0cbe83e685e1541fb58d0af0a42d6
Linda Adisa Masitsa <lindaadisa022@gmail.com>
2022-06-02 17:28:32 -0700

    Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -32,8 +32,8 @@ int main(void)
 
 										printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
 
-											printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f)
+											printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
 
-													return (0);
+												return (0);
 
 }
