50efbf36567a2c870561c0426959a4f143286ed5
Linda Adisa Masitsa <lindaadisa022@gmail.com>
2022-06-09 10:55:24 -0700

    task 1

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -2,11 +2,11 @@
 
 /**
  * _isdigit - Checks for a digit (0-9).
- * @x: The number to be checked.
+ * @c: The number to be checked.
  * Return: 1 if the number is a digit, 0 otherwise.
  */
 
-int _isdigit(int x)
+int _isdigit(int c)
 
 {
 
