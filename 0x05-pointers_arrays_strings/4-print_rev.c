c22dfa3b99c66297347b14b0dbfe9821b34bf941
Linda Adisa Masitsa <lindaadisa022@gmail.com>
2022-06-13 11:07:32 -0700

    task 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -9,13 +9,27 @@ void print_rev(char *s)
 
 {
 
-	int len = 0, index;
+	int i = 0;
 
-	while (s[index++])
-		len++;
 
-	for (index = len - 1; index >= 0; index--)
-		_putchar(s[index]);
+	while (s[i] != '\0')
+
+	{
+
+		i++;
+
+	}
+
+	i--;
+
+	while (i >= 0)
+
+	{
+
+		_putchar(s[i]);
+
+		i--;
+	}
 
 	_putchar('\n');
 
