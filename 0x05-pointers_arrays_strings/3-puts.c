a53a29ee4efb755bd13e73c16fd7b9aab16278c9
Linda Adisa Masitsa <lindaadisa022@gmail.com>
2022-06-13 09:44:16 -0700

    task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string and a newline
+ * @str:input
+ * Return:void
+ */
+
+void _puts(char *str)
+
+{
+
+	int i = 0;
+
+	while (str[i] != '\0')
+
+	{
+
+		_putchar(str[i]);
+
+		i++;
+
+	}
+
+	_putchar('\n');
+
+}
