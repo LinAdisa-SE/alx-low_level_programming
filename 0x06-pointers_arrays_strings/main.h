d7b9e1befcef59d4e922261f62f9d190afee7ca1
Linda Adisa Masitsa <lindaadisa022@gmail.com>
2022-06-15 12:29:32 -0700

    task 9

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -12,10 +12,8 @@ char *_strpbrk(char *s, char *accept);
 
 char *_strstr(char *haystack, char *needle);
 
-void print_chessboard(char (*a)[8]);
-
-void print_diagsums(int *a, int size);
-
 void set_string(char **s, char *to);
 
 char *rot13(char *);
+
+void print_number(int n);
