a67bb5a2a66974d33109f6a9d3c5e9231ccee841
Linda Adisa Masitsa <lindaadisa022@gmail.com>
2022-07-04 03:08:26 -0700

    main

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,30 +1,4 @@
-#ifndef DOG_H
-#define DOG_H
-
-/**
- * struct dog - structure for dog variables
- * @name:name
- * @age:age of dog
- * @owner:name of owner
- */
-
-typedef struct dog
-
-{
-	char *name;
-
-	float age;
-
-	char *owner;
-
-} dog_t;
-
+int _putchar(char c);
 void init_dog(struct dog *d, char *name, float age, char *owner);
-
 void print_dog(struct dog *d);
-
-dog_t *new_dog(char *name, float age, char *owner);
-
 void free_dog(dog_t *d);
-
-#endif
